525b7710416556464402a0c27d409ff69605c4e2
Charles Duruaku <charlesrichson@gmail.com>
2022-08-23 15:57:04 +0100

    ads

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-for ascii_num in range(122, 96, -1):
-	if ascii_num % 2 == 1:
-		ascii_num = ascii_num - 32
-	print("{:c}".format(ascii_num), end='')
+for char in range(122, 96, -1):
+    print("{}".format(chr(char) if char % 2 == 0 else chr(char - 32)), end="")
