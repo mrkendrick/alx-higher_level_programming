0f9be3f4d8cd7e7b96eb6ea7fdea33d954e3e661
Charles Duruaku <charlesrichson@gmail.com>
2022-08-23 15:54:08 +0100

    ads

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,8 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 def uppercase(str):
-    for i in str:
-        if ord(i) >= ord('a') and ord(i) <= ord('z'):
-            i = chr(ord(i) - 32)
-    print("{:s}".format(i), end="")
-    print()
+    for ch in str:
+        if ord(ch) >= 97 and ord(ch) <= 122:
+            ch = ord(ch) - 32
+            ch = chr(ch)
+        print("{}".format(ch), end="")
+    print("")
